c3e6dfbd82a550578165f31a7d8b7ef71e2ff514
oussama7007 <oussamatachakourt@gmail.com>
2025-11-15 16:34:49 +0100

    Auto update: 2025-11-15 16:34:49

--- ex00/Fixed.cpp
+++ ex00/Fixed.cpp
@@ -6,7 +6,22 @@
 /*   By: oait-si- <oait-si-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/12 10:16:52 by oait-si-          #+#    #+#             */
-/*   Updated: 2025/11/12 11:41:43 by oait-si-         ###   ########.fr       */
+/*   Updated: 2025/11/15 16:20:26 by oait-si-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Fixed.h"
+
+
+const int Fixed::fractional_bit = 8;
+
+Fixed::Fixed() : value(0)
+{
+        std::cout << "Default constructor called" << std::endl;
+} 
+
+Fixed::Fixed(const Fixed& other)
+{
+        std::cout << "Copy constructor called" << std::endl;
+        this->value = other.value 
+}
--- ex00/Fixed.h
+++ ex00/Fixed.h
@@ -6,7 +6,7 @@
 /*   By: oait-si- <oait-si-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/13 12:20:24 by oait-si-          #+#    #+#             */
-/*   Updated: 2025/11/13 12:25:30 by oait-si-         ###   ########.fr       */
+/*   Updated: 2025/11/15 16:18:15 by oait-si-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,20 @@
 #ifndef FIXED_H
 #define FIXED_H
 
+#include <iostream>
+
 class Fixed
 {
     private:
-        int fixed
-    public:  
+        int value;
+        static const int fractional_bit;
+    public:
+        Fixed();
+        Fixed(const Fixed& other);
+        Fixed& operator=(const Fixed& other);
+        ~Fixed();
+        void getRawBits(void) const;
+        void setRawBits(int const raw);
 };
 
 
