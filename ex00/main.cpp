285fb548e6316c60cd6cdb59590dfd73287bde0e
oussama7007 <oussamatachakourt@gmail.com>
2025-11-15 14:34:46 +0100

    Auto update: 2025-11-15 14:34:46

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,16 +6,54 @@
 /*   By: oait-si- <oait-si-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/13 16:33:10 by oait-si-          #+#    #+#             */
-/*   Updated: 2025/11/13 16:34:26 by oait-si-         ###   ########.fr       */
+/*   Updated: 2025/11/15 14:33:25 by oait-si-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+
+
+#include <iostream>
 
 class Number 
 {
     private:
       int   _value;
     public  :
-        Number()
+        Number() : _value(0) {
+          std::cout << "the constructor has been called" ;
+        }
         
+
+        Number(const Number& other) : _value(other._value) {
+            std::cout << "copy constructor called";
+        }
+        
+        //copy assignment operator
+        Number& operator=(const Number&other )
+        {
+              if(this != &other)
+              {
+                  _value = other._value;
+              }
+        }
+        ~Number() 
+        {
+            std::cout << "the destructor has benn called";
+        }
+
+        int getvalu() const 
+        {
+            return _value;
+        }
+
+        void  setvalue(int value)
+        {
+            _value = value;
+        }
+};
+
+int main()
+{
+    Number obj1(100);
+    return 0;
 }
