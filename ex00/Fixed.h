477b6d56f4238ead1ba6e9c3c5664958197c2c4a
oussama7007 <oussamatachakourt@gmail.com>
2025-11-16 12:53:37 +0100

    Auto update: 2025-11-16 12:53:37

--- ex00/Fixed.h
+++ ex00/Fixed.h
@@ -6,7 +6,7 @@
 /*   By: oait-si- <oait-si-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/13 12:20:24 by oait-si-          #+#    #+#             */
-/*   Updated: 2025/11/15 16:18:15 by oait-si-         ###   ########.fr       */
+/*   Updated: 2025/11/16 12:42:30 by oait-si-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,10 +23,10 @@ class Fixed
         int value;
         static const int fractional_bit;
     public:
-        Fixed();
-        Fixed(const Fixed& other);
-        Fixed& operator=(const Fixed& other);
-        ~Fixed();
+        Fixed(); // d constructor  
+        Fixed(const Fixed& other); // cp contructor 
+        Fixed& operator=(const Fixed& other);   // assignemnt constructor 
+        ~Fixed(); // destruc
         void getRawBits(void) const;
         void setRawBits(int const raw);
 };
