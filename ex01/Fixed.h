358ddb0362ff9a282055283741eccfb430843ec3
oussama7007 <oussamatachakourt@gmail.com>
2025-11-18 14:43:15 +0100

    Auto update: 2025-11-18 14:43:15

--- ex01/Fixed.h
+++ ex01/Fixed.h
@@ -6,7 +6,28 @@
 /*   By: oait-si- <oait-si-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/18 13:16:02 by oait-si-          #+#    #+#             */
-/*   Updated: 2025/11/18 13:16:03 by oait-si-         ###   ########.fr       */
+/*   Updated: 2025/11/18 14:28:54 by oait-si-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef FIXED_H
+#define FIXED_H
+
+#include <iostream>
+
+
+
+class Fixed 
+{
+        private:
+            const int value;
+            const static int fractional_bits;
+        public:
+            Fixed(const int value); // to converts it to the corresponding fixed-point value.
+            Fixed(const float value); // to converts it to corresponding fixed point value 
+            float toFloat( void ) const ;
+            int toInt( void ) const ;
+}
+
+
+#endif 
