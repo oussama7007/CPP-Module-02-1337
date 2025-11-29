13f448eee6ced35819f9bfd7a213d23ea4affe67
Oussama Ait Si Hamou <oussamatachakourt@gmail.com>
2025-11-29 10:53:26 +0100

    d

--- ex03/Point.cpp
+++ ex03/Point.cpp
@@ -6,7 +6,7 @@
 /*   By: oait-si- <oait-si-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/29 09:27:03 by oait-si-          #+#    #+#             */
-/*   Updated: 2025/11/29 09:41:21 by oait-si-         ###   ########.fr       */
+/*   Updated: 2025/11/29 10:00:58 by oait-si-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,18 @@
 #ifndef POINT_CPP
 #define POINT_CPP
 
-class 
+class Point
+{
+    private:
+        Fixed const x;
+        Fixed const y;
+    public:
+        Point(); // default constructor
+        Point(const Point& other); // cp constructor
+        Point()
+        Point& operator=(const Point& other); // cp assignement
+        ~Point(); // destructor;
+};
   
 
 #endif 
