b24566a4151e12bf086f875d3f6ecac51b1d4252
Oussama Ait Si Hamou <oait-si-@e2r13p4.1337.ma>
2025-11-29 09:20:54 +0100

    still

--- ex02/Fixed.cpp
+++ ex02/Fixed.cpp
@@ -6,18 +6,59 @@
 /*   By: oait-si- <oait-si-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/25 16:58:37 by oait-si-          #+#    #+#             */
-/*   Updated: 2025/11/27 18:51:41 by oait-si-         ###   ########.fr       */
+/*   Updated: 2025/11/29 09:20:41 by oait-si-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 
 #include "Fixed.h"
 
+// Default constructor called
+// Int constructor called
+// Float constructor called
+// Copy constructor called
+// Copy assignment operator called
+// Float constructor called
+// Copy assignment operator called
+// Destructor called
+// a is 1234.43
+// b is 10
+// c is 42.4219
+// d is 10
+// a is 1234 as integer
+// b is 10 as integer
+// c is 42 as integer
+// d is 10 as integer
+// Destructor called
+// Destructor called
+// Destructor called
+// Destructor called
 
 
 const int Fixed::fractional_bits = 8;
 
-Fixed::Fixed(): value(0) {}
+Fixed::Fixed(): value(0) {
+    std::cout << "Default constructor called" << std::endl;
+}
+
+Fixed::Fixed(const Fixed& other)
+{
+    std::cout << "Copy constructor called" << std::endl;
+    *this = other;
+}
+
+Fixed& Fixed::operator=(const Fixed& other)
+{
+    if (this != &other)
+    {
+        this->value = other.value;
+    }
+    return *this;
+}
+
+Fixed::~Fixed() 
+{}
+
 
 Fixed::Fixed(const int value)
 {
@@ -29,14 +70,14 @@ Fixed::Fixed(const float value)
     this->value = roundf(value * (1 << fractional_bits));
 }
 
-            // void    setRawBits(int const raw);
-            // int     getRawBits( void ) const ;
-            // float toFloat( void ) const ;
-            // int     toInt( void ) const ; 
-
 float Fixed::toFloat( void ) const
 {
-    
+    return (float)value / (1 << fractional_bits);
+}
+
+int     Fixed::toInt(void) const
+{
+    return value >> fractional_bits;
 }
 
 Fixed Fixed::operator+(const Fixed& obj) const
@@ -56,7 +97,7 @@ Fixed Fixed::operator-(const Fixed& obj) const
 Fixed Fixed::operator*(const Fixed& obj) const
 {
     Fixed result;
-    result.value = (this->value * obj.value) / (1 << fractional_bits);
+    result.value = (long long)(this->value * obj.value) / (1 << fractional_bits);
     return result;
 }
 
@@ -154,4 +195,10 @@ const Fixed& Fixed::min(const Fixed& a, const Fixed& b)
         return a;
     else 
         return b;
+}
+
+std::ostream& operator<<(std::ostream& out, const Fixed& obj)
+{
+    out << obj.toFloat();
+    return out;
 }
